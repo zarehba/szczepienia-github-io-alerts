6908ccbbd220ff5781bf5730de46a7464eebb4ca
zarehba <zarehba@protonmail.com>
2021-05-02 13:14:48 +0200

    add saving telegram bot's chatIds

--- src/notifyTelegram.js
+++ src/notifyTelegram.js
@@ -1,10 +1,21 @@
+import fs from "fs";
 import fetch from "node-fetch";
+const jsonPath = "./logs/chatIds.json";
 
 const notifyTelegram = async (message) => {
+  // bot subscribers so far
+  const savedData = JSON.parse(fs.readFileSync(jsonPath, "utf-8"));
+  // get new bot subscribers from last 24hours
   const { result } = await getChatUpdates();
+
   const chatIds = [
-    ...new Set(result.map((chatUpdate) => chatUpdate.message.chat.id)),
+    ...new Set(
+      savedData.concat(result.map((chatUpdate) => chatUpdate.message.chat.id))
+    ),
   ];
+
+  // save updated list of bot subscibers
+  fs.writeFileSync(jsonPath, JSON.stringify(chatIds));
   // send the message to all chats
   chatIds.forEach((chatId) => sendTelegramMsg(message, chatId));
 };
